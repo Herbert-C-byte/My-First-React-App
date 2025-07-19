0e350296225c69328384fedd2253e1517cea7ea1
Herbert Costa <epsylon344@gmail.com>
2025-07-19 02:25:00 +0100

    'Cassio/Final-secondSide-version'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import Main from './components/main.jsx'
+import NavBar from './components/Navbar.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+      <>
+        <NavBar />
+        <Main />
+      </>
+
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../index.css'
+
+function NavBar () {
+    return (
+        <header>
+            <nav>
+                <img src="../public/icons8-react-50.png" alt="React Logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+
+    ) 
+}
+
+export default NavBar
